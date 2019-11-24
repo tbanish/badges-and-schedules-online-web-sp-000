facf889b63aca3cac7009e3321f651a6d988b4ad
Tom Banish <tombanish@gmail.com>
2019-11-24 01:20:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  speakers.each{|name| 
+  speakers.each{|name| puts "Hello, my name is #{name}."}
 end
 
 
