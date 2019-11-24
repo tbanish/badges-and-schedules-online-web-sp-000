f23777eaf6db88bb5ab6ca38caf743c2f8d29ae9
Tom Banish <tombanish@gmail.com>
2019-11-24 01:40:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,7 @@ end
 
 
 def batch_badge_creator(speakers)
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   count = 0
   while count <= speakers.length-1
     puts "Hello, my name is #{speakers[count]}!"
